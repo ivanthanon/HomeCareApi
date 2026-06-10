1e8939c31a5127fe065af28e705d6d2ddf6c1b04
ithanon <ivanmoreno199518@gmail.com>
2026-06-10 21:41:13 +0100

    Update dependencies due to problems in test running

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
-
-    environment: 'jsdom', 
-    
+    environment: 'node',
+    globals: true,
+    include: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
     coverage: {
-      provider: 'v8', // o 'istanbul'
+      provider: 'v8',
       reporter: ['text', 'json', 'html'],
     },
-    
-    globals: true,
-    include: ['**/*.spec.ts', '**/*.e2e-spec.ts'], 
   },
 });
