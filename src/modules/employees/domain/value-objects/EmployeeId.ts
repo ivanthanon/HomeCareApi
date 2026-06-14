ed13b83f40c988e54c10416ca9b6f1dbfdb385d8
ithanon <ivanmoreno199518@gmail.com>
2026-06-14 17:42:00 +0100

    Extract method for living in the same level of abstraction

--- src/modules/employees/domain/value-objects/EmployeeId.ts
+++ src/modules/employees/domain/value-objects/EmployeeId.ts
@@ -3,11 +3,15 @@ export class EmployeeId {
     private constructor(readonly value: string) {}
 
     public static create(value: string): Result<EmployeeId, Error> {
-        const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
-        if (uuidRegex.test(value) == false) {
+        if (this.IsAValidGuid(value) == false) {
             return Err(new Error('Invalid UUID format'));
         }
         
         return Ok(new EmployeeId(value));
+    }
+
+    private static IsAValidGuid(value: string): boolean {
+        const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+        return uuidRegex.test(value);
     }
 }
