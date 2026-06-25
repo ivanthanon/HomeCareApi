44ccc90dac8e77bafff2c25f34bf5b7f863762fc
ithanon <ivanmoreno199518@gmail.com>
2026-06-25 11:05:47 +0100

    Add contract test for in memory repository against sql server repository

---
+++ tests/modules/employees/infrastructure/contract/InMemoryUserRepositoryContract.spec.ts
@@ -0,0 +1,25 @@
+import { EmployeeRepository } from "src/modules/employees/domain/repositories/employee.repository";
+import { EmployeeRepositoryContractTest } from "tests/modules/employees/infrastructure/contract/EmployeeRepositoryContractTest";
+import { EmployeeInMemoryRepository } from "../../application/create-employee/EmployeeInMemoryRepository";
+import { Employee } from "src/modules/employees/domain/employee";
+
+class InMemoryUserRepositoryContract extends EmployeeRepositoryContractTest {
+
+    public employeeInMemoryRepository!: EmployeeInMemoryRepository;
+
+    protected createRepository(): EmployeeRepository {
+        this.employeeInMemoryRepository = new EmployeeInMemoryRepository();
+        return this.employeeInMemoryRepository;
+    }
+
+    protected async cleanUp(): Promise<void> { 
+        this.employeeInMemoryRepository.employeeList = [];
+     }
+
+    protected async customAssert(expectedEmployee: Employee): Promise<void> {
+        expect(this.employeeInMemoryRepository.employeeList).toHaveLength(1);
+        expect(this.employeeInMemoryRepository.employeeList[0]).toMatchObject(expectedEmployee);
+    }
+}
+
+new InMemoryUserRepositoryContract().runContractTest();
