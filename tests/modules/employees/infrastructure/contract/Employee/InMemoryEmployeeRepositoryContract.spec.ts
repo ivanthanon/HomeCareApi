c388fb50245005a106239b9f94cea9e8df0aeeac
ithanon <ivanmoreno199518@gmail.com>
2026-07-28 16:35:13 +0100

    rename folder

--- tests/modules/employees/infrastructure/contract/Employee/InMemoryEmployeeRepositoryContract.spec.ts
+++ tests/modules/employees/infrastructure/contract/Employee/InMemoryEmployeeRepositoryContract.spec.ts
@@ -1,5 +1,5 @@
 import { EmployeeRepository } from "src/modules/employees/domain/repositories/employee.repository";
-import { EmployeeRepositoryContractTest } from "tests/modules/employees/infrastructure/contract/Employee/EmployeeRepositoryContractTest";
+import { EmployeeRepositoryContractTest } from "tests/modules/employees/infrastructure/contract/employee/EmployeeRepositoryContractTest";
 import { EmployeeInMemoryRepository } from "../../helpers/fake/EmployeeInMemoryRepository";
 import { Employee } from "src/modules/employees/domain/employee";
 
